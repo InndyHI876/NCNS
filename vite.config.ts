85f11b2c5dca774f9e52d99ffe51d0cfc1abd7a4
InndyHI876 <inndyhi2002@gmail.com>
2025-07-04 08:55:44 -0500

    modified vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/NCNS/',
   plugins: [react()],
   optimizeDeps: {
     include: ['maplibre-gl', 'react-map-gl']
